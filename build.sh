5305639ffada7fc453ccdc8d4a86d8c249d763aa
Sergey Bochenkov <bachan@j3qq4.org>
2012-06-28 17:52:49 +0700

    server-honest

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 gcc -O3 -march=native -mtune=native server.c -lev -o server
+gcc -O3 -march=native -mtune=native server-honest.c -lev -o server-honest
+
 gcc -O3 -march=native -mtune=native client.c -lev -o client
 
 gcc -O3 -march=native -mtune=native thread.c -lev -lpthread -o thread
