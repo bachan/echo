b6561b3521a5084e0be491c7f43d3a5fa1e9b088
Sergey Bochenkov <bachan@j3qq4.org>
2012-06-28 15:48:54 +0700

    march

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-gcc -O3  server.c -lev -o server
-gcc -O3 client.c -lev -o client
+gcc -O3 -march=native -mtune=native server.c -lev -o server
+gcc -O3 -march=native -mtune=native client.c -lev -o client
 
-gcc -O3 thread.c -lev -lpthread -o thread
-gcc -O3 thread-honest.c -lev -lpthread -o thread-honest
+gcc -O3 -march=native -mtune=native thread.c -lev -lpthread -o thread
+gcc -O3 -march=native -mtune=native thread-honest.c -lev -lpthread -o thread-honest
 
