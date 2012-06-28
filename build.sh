c687d613ca86145b76c1471f2aab9d6215e7ede4
Sergey Bochenkov <bachan@j3qq4.org>
2012-06-28 15:47:53 +0700

    thread-honest

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-gcc -O3 server.c -lev -o server
+gcc -O3  server.c -lev -o server
 gcc -O3 client.c -lev -o client
 
 gcc -O3 thread.c -lev -lpthread -o thread
+gcc -O3 thread-honest.c -lev -lpthread -o thread-honest
 
